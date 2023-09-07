67154cf91454f22854b9b5cb131f4df589bb88f2
Solarahamza <solarahamza98@gmail.com>
2023-09-07 14:32:25 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%lu/n"sizeof(char));
-	printf("%lu/n"sizeof(int));
-	printf("%lu/n"sizeof(long int));
-	printf("%lu/n"sizeof(long long int));
-	printf("%lu/n"sizeof(float));
+	printf("%u/n"sizeof(char));
+	printf("%u/n"sizeof(int));
+	printf("%u/n"sizeof(long int));
+	printf("%u/n"sizeof(long long int));
+	printf("%u/n"sizeof(float));
 		return (0);
 }
