0a4540ae3c88ada73e5e495e325df2f6047c4274
Solarahamza <solarahamza98@gmail.com>
2023-09-07 15:42:03 -0700

    printf(%u/nsizeof(int));

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%u/n"sizeof(char));
-	printf("%u/n"sizeof(int));
-	printf("%u/n"sizeof(long int));
-	printf("%u/n"sizeof(long long int));
-	printf("%u/n"sizeof(float));
+	printf("Size of a char: %lu byte/n", sizeof(char));
+	printf("Size of an int: %lu byte/n", sizeof(int));
+	printf("Size of a long int: %lu byte /n", sizeof(long int));
+	printf("Size of a long long int: %lu byte /n", sizeof(long long int));
+	printf("Size of a float: %lu byte /n", sizeof(float));
 		return (0);
 }
