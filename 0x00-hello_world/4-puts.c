d801c680a00702a72c10a6f9f349313f4eb02f3e
Solarahamza <solarahamza98@gmail.com>
2023-09-07 14:09:45 -0700

    fixed stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/** main - Entry point
+/**
+ * main - Entry point
  *
  * Description: use of puts function
  *
- * Retrurn (0) = Success
+ * Return: Always 0 (Success)
  */
 
 int main(void)
