a5ea15a6c8e5e0e38fe2f235ed54b3da93870835
Solarahamza <solarahamza98@gmail.com>
2023-09-08 17:54:08 -0700

    yo

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,14 @@ int main(void)
 {
 	char ch = 'a';
 
-		while (ch <='z')
-		{ if ( ch =='q' || ch =='e')
+		while (ch <= 'z')
+		{
+			if (ch == 'q' || ch == 'e')
 			ch++;
-		       	putchar(ch);
+			putchar(ch);
 			ch++;
 		}
+
 		putchar('\n');
 
 		return (0);
