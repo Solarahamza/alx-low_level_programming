1d1151d00f1e384e828fa67fba03cc6bcc26347c
Solarahamza <solarahamza98@gmail.com>
2023-09-08 17:05:31 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{ char ch = 'a'
+	while (ch <= 'z')
+	{putchar(ch);
+		ch++
+	}
+	putch("\n");
+
+	return (0);
+}
