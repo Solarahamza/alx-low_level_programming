b70a1496d13337eee01f2342c0406a0adb451d85
Solarahamza <solarahamza98@gmail.com>
2023-09-08 18:49:20 -0700

    gh

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  a program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  a program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+
+
+        return (0);
+}
