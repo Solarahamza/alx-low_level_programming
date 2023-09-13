a5e40657fb078ebd1f578126622e410212ebfc3a
Solarahamza <solarahamza98@gmail.com>
2023-09-13 05:29:35 -0700

    fourth task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,10 +13,10 @@
 
 int _isalpha(int c)
 {
-	for (c = 65 ; c <= 90 ; c++)
+	if (c >= 65 && c <= 90)
 		return (1);
 
-	for (c = 97 ; c <= 122 ; c++)
+	if (c >= 97 && c <= 122)
 		return (1);
 
 	return (0);
