93793023e8d15be4b57baefcf28bba10e9dac967
Solarahamza <solarahamza98@gmail.com>
2023-09-15 18:44:29 -0700

    sh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,10 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
+			{
 				_putchar(32);
+			}
 		_putchar(92);
 		_putchar('\n');
 		}
