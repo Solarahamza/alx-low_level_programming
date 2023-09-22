36acc46353bdfce9ece0efc35f9e84175285dfd0
Solarahamza <solarahamza98@gmail.com>
2023-09-21 19:07:14 -0700

    rere

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cap_string -  a function that capitalizes all words of a string
+ *
+ * @str: string parameter
+ *
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+}
